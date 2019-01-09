ff68146e3d03af6d42c4b269904fed7b1d38a0d4
goshut <952918451@qq.com>
2019-01-09 19:26:21 +0800

    删除111

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^index/$', views.index),
+    # url(r'^say/$', views.say),
+    url(r'^hand/(?P<b>\d{3})/(?P<a>[A-z]{3})/$', views.hand),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse('hehehhehe')
+
+
+def hand(request, a, b):
+    print(a)
+    print(b)
+    return HttpResponse('\t'+a+'\t'+b)
+
+
+def post(request):
+    json1 = request.body
